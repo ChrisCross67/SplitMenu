3709585e30a5fa7e2fbd6ab0ab8493ab8006af79
Christopher <c.sauer@sra-instruments.com>
2016-04-05 15:06:10 +0200

    Initial Commit

---
+++ SplitMenu/SplitMenu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SplitMenu
+{
+    public class SplitMenu : ItemsControl
+    {
+        public SplitMenu()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is expanded.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is expanded; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsExpanded.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SplitMenu), new PropertyMetadata(false));
+
+
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(SplitMenu), new PropertyMetadata(48d));
+
+
+        /// <summary>
+        /// Returns true is item is a DragDockPanel.
+        /// </summary>
+        /// <param name="item">The item to check.</param>
+        /// <returns>True is item is a DragDockPanel.</returns>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is SplitMenuItem;
+        }
+
+        /// <summary>
+        /// Returns a new drag dock panel.
+        /// </summary>
+        /// <returns>A new drag dock panel.</returns>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new SplitMenuItem();
+        }
+
+    }
+}
---
+++ SplitMenu/SplitMenuItem.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SplitMenu
+{
+    public class SplitMenuItem : RadioButton
+    {
+        #region Dependencies
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(object), typeof(SplitMenuItem), new PropertyMetadata(null));
+
+        #endregion Dependencies
+
+        public SplitMenuItem()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        internal SplitMenuItem ParentItem { get; set; }
+
+        public ItemsControl SubItems
+        {
+            get { return (ItemsControl)GetValue(SubItemsProperty); }
+            set { SetValue(SubItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SubItems.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SubItemsProperty =
+            DependencyProperty.Register("SubItems", typeof(ItemsControl), typeof(SplitMenuItem), new PropertyMetadata(null, OnSubItemsChanged));
+
+        private static void OnSubItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var splitMenuItem = d as SplitMenuItem;
+            if (splitMenuItem == null)
+                return;
+            //SplitViewNavButtonStyle
+            if (e.OldValue != null)
+            {
+                var oldItemsControl = e.NewValue as ItemsControl;
+                //((INotifyCollectionChanged)oldItemsControl.Items).CollectionChanged -= OnSubItemCollectionChanged;
+
+                var parentPopup = oldItemsControl.Parent as Popup;
+                if(parentPopup != null)
+                {
+                    parentPopup.Child = null;
+                }
+            }
+            if (e.NewValue != null)
+            {
+                var itemsControl = e.NewValue as ItemsControl;
+                if (itemsControl == null)
+                    return;
+                itemsControl.ItemsPanel = GetItemsPanelTemplate();
+                //((INotifyCollectionChanged)itemsControl.Items).CollectionChanged += OnSubItemCollectionChanged;
+                //new ItemsPanelTemplate(new StackPanel());
+                var popup = new Popup
+                {
+                    StaysOpen = false,
+                    PlacementTarget = splitMenuItem,
+                    Placement = PlacementMode.Right,
+                    Child = itemsControl
+                };
+                var binding = new Binding
+                {
+                    Source = splitMenuItem,
+                    Path = new PropertyPath(nameof(IsChecked)),
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                    NotifyOnSourceUpdated = true
+                };
+                BindingOperations.SetBinding(popup, Popup.IsOpenProperty, binding);
+
+                //var subItemStyle = Application.Current.Resources["SplitViewMenuButtonStyle"] as Style;
+                //if(subItemStyle != null)
+                //    splitMenuItem.Style = subItemStyle;
+            }
+        }
+
+        private static void OnSubItemCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var subItem = sender as SplitMenuItem;
+            if (subItem == null)
+                return;
+            subItem.GroupName = "Sub 1";
+        }
+
+        private static ItemsPanelTemplate GetItemsPanelTemplate()
+        {
+            string xaml = @"<ItemsPanelTemplate
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>
+                                <StackPanel/>
+                            </ItemsPanelTemplate>";
+            return XamlReader.Parse(xaml) as ItemsPanelTemplate;
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        /// <value>
+        /// The icon.
+        /// </value>
+        public object Icon
+        {
+            get { return (object)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        #endregion Properties
+    }
+}
